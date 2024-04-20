74818f141d25661cebc96342b7035b3c96c80ab4
Sergio Stefanizzi <sergio.stef25@gmail.com>
2024-04-20 12:09:03 +0200

    add knn

--- ulivi_polignano_monopoli.m
+++ ulivi_polignano_monopoli.m
@@ -107,3 +107,27 @@ dataset = table(ndviImg(notTerrainIdx),evi2Img(notTerrainIdx),cireImg(notTerrain
 
 %% Correlazione
 [XTrainSetNew,XTestSetNew] = correlationFeatureSelection(XTrainSet,XTrainSet);
+
+%% Training e Testing KNN
+mdl = fitcknn(XTrainSetNew(:,1:6),XTrainSetNew(:,"labels"),'NumNeighbors',5,'Standardize',1);
+cvmdl = crossval(mdl); %10-fold
+
+[Ypredicted,score,cost] = predict(cvmdl.Trained{1},XTestSetNew(:,1:6));
+
+C = confusionmat(XTestSetNew{:,"labels"},Ypredicted);
+figure
+cm = confusionchart(C,cultNameAndCount(:,1));
+%% AUC curve
+AUC = zeros(6);
+legends = cell(6,1);
+figure
+hold on
+for i=1:6
+    [X,Y,T,AUC(i)] = perfcurve(XTestSetNew{:,"labels"},score(:,1),i);
+    plot(X,Y)
+    legends{i} = sprintf('AUC for %s class: %.3f', cultNameAndCount{i,1}, AUC(i));
+end
+legend(legends, 'location', 'southeast')
+    xlabel('False positive rate'); ylabel('True positive rate');
+    title('ROC for Classification by KNN')
+hold off
